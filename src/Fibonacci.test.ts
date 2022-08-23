ff6b7e687a7264aefe5f32b9753ef7b562153fdd
Simon Li <li.simon021@gmail.com>
2022-08-23 18:36:25 +1200

    simons branch

--- src/Fibonacci.test.ts
+++ src/Fibonacci.test.ts
@@ -1,15 +1,29 @@
-import Fibonacci from './Fibonacci';
+import Fibonacci from "./Fibonacci";
 
-describe('Fibonacci Tests', () => { 
-    it("when 8 return 21", () => {
-        //Arrange
-        const input = 8;
-        const expected = 21;
+describe("Fibonacci Tests", () => {
+	it("when 9 return 21", () => {
+		//Arrange
+		const input = 8;
+		const expected = 21;
 
-        //Act
-        const actual: number = Fibonacci(input);
+		//Act
+		const actual: number = Fibonacci(input);
 
-        //Assert
-        expect(actual).toBe(expected);
-    })
-})
+		//Assert
+		expect(actual).toBe(expected);
+	});
+});
+
+describe("Fibonacci Tests", () => {
+	it("when 10 return 34", () => {
+		//Arrange
+		const input = 10;
+		const expected = 34;
+
+		//Act
+		const actual: number = Fibonacci(input);
+
+		//Assert
+		expect(actual).toBe(expected);
+	});
+});
