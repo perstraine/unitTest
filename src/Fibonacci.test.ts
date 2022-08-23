3113ad8d07d99b4b9ff7dc26b13c3d13a31ea06b
perstraine <41206120+perstraine@users.noreply.github.com>
2022-08-23 18:12:02 +1200

    added failing test actually

--- src/Fibonacci.test.ts
+++ src/Fibonacci.test.ts
@@ -14,7 +14,7 @@ describe('Fibonacci Tests', () => {
     })
     it("when 6 return 21", () => {
         //Arrange
-        const input = 9;
+        const input = 6;
         const expected = 21;
 
         //Act
