8b22fa1ba9a16c53ac796b11cec88db7df795911
Simon Li <li.simon021@gmail.com>
2022-08-23 18:50:28 +1200

    Simons 3rd commit

--- src/Fibonacci.ts
+++ src/Fibonacci.ts
@@ -1,29 +1,7 @@
 const Fibonacci = (input: number): number => {
-<<<<<<< HEAD
 	let first = 0;
-=======
-    let first = 0;
-    let second = 1;
-    let temp = 0;
-    if (input == 1) {
-        return 0;
-    } else if (input == 2) {
-        return 1;
-    } else {
-        for (let index = 0; index < input -2; index++) {
-            temp = first + second;
-            first = second;
-            second = temp;
-        }
-        return temp
-    }
-}
->>>>>>> e1071f448041be5e7a95e97affaf54e30741629e
-
 	let second = 1;
-
 	let temp = 0;
-
 	if (input == 1) {
 		return 0;
 	} else if (input == 2) {
@@ -31,12 +9,9 @@ const Fibonacci = (input: number): number => {
 	} else {
 		for (let index = 0; index < input - 2; index++) {
 			temp = first + second;
-
 			first = second;
-
 			second = temp;
 		}
-
 		return temp;
 	}
 };
