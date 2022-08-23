58088d5bb2e5a018dfca49b2c1e0a779c6a9a525
perstraine <41206120+perstraine@users.noreply.github.com>
2022-08-23 18:09:00 +1200

    added failing test

--- src/Fibonacci.test.ts
+++ src/Fibonacci.test.ts
@@ -12,4 +12,15 @@ describe('Fibonacci Tests', () => {
         //Assert
         expect(actual).toBe(expected);
     })
+    it("when 6 return 21", () => {
+        //Arrange
+        const input = 9;
+        const expected = 21;
+
+        //Act
+        const actual: number = Fibonacci(input);
+
+        //Assert
+        expect(actual).toBe(expected);
+    })
 })
